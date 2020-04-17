59e83c153b27724e9179ed794b4e5923631c8dad
Satriyo Pamungkas <satriyopamungkas@gmail.com>
2020-04-17 19:51:24 +0700

    example range

--- examples/range/range.go
+++ examples/range/range.go
@@ -1,15 +1,15 @@
-// _range_ iterates over elements in a variety of data
-// structures. Let's see how to use `range` with some
-// of the data structures we've already learned.
-
+//_range_ akan mengiterasi elemen-elemen dalam
+// berbagai struktur data. Mari kita lihat penggunaannya
+// dengan struktur data yang sudah kita pelajari.
 package main
 
 import "fmt"
 
 func main() {
 
-	// Here we use `range` to sum the numbers in a slice.
-	// Arrays work like this too.
+	// Di contoh ini kita menggunakan `range` untuk
+	// menjumlahkan angka dalam sebuah _slice_.
+	// teknik ini bisa juga digunakan untuk _array_.
 	nums := []int{2, 3, 4}
 	sum := 0
 	for _, num := range nums {
@@ -17,31 +17,32 @@ func main() {
 	}
 	fmt.Println("sum:", sum)
 
-	// `range` on arrays and slices provides both the
-	// index and value for each entry. Above we didn't
-	// need the index, so we ignored it with the
-	// blank identifier `_`. Sometimes we actually want
-	// the indexes though.
+	// `range` pada _array_ dan _slice_ menyediakan
+	// index dan value untuk setiap elemen. Contoh di atas
+	// kita tidak membutuhkan index, sehingga kita mengabaikannya
+	// dengan _blank identifier_ yaitu `_`. Tapi kadang kita membutuhkan
+	// index.
 	for i, num := range nums {
 		if num == 3 {
 			fmt.Println("index:", i)
 		}
 	}
 
-	// `range` on map iterates over key/value pairs.
+	// `range` pada map akan mengiterasi pasangan key/value.
 	kvs := map[string]string{"a": "apple", "b": "banana"}
 	for k, v := range kvs {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
 
-	// `range` can also iterate over just the keys of a map.
+	// `range` bisa digunakan juga untuk hanya mengiterasi key saja
+	// pada sebuah map.
 	for k := range kvs {
 		fmt.Println("key:", k)
 	}
 
-	// `range` on strings iterates over Unicode code
-	// points. The first value is the starting byte index
-	// of the `rune` and the second the `rune` itself.
+	// `range` pada string akan mengiterasi _Unicode code point_.
+	// Nilai pertama pada index byte dari `rune` dan nilai kedua
+	// adalah `rune` nya itu sendiri.
 	for i, c := range "go" {
 		fmt.Println(i, c)
 	}
