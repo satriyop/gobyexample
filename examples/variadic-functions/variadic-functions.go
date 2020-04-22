53f8c827381c22df27c8c27941dc5572e0f0ad6a
Satriyo Pamungkas <satriyopamungkas@gmail.com>
2020-04-22 17:41:16 +0700

    example : variadic func

--- examples/variadic-functions/variadic-functions.go
+++ examples/variadic-functions/variadic-functions.go
@@ -1,14 +1,12 @@
 // [_Variadic functions_](http://en.wikipedia.org/wiki/Variadic_function)
-// can be called with any number of trailing arguments.
-// For example, `fmt.Println` is a common variadic
-// function.
-
+// bisa dipanggil dengan sejumlah argumen yang bervariasi.
+// `fmt.Println` adalah salah satu contoh dari fungsi variadic.
 package main
 
 import "fmt"
 
-// Here's a function that will take an arbitrary number
-// of `int`s as arguments.
+// Ini adalah sebuah contoh fungsi variadic yang akan menerima sejumlah `int`
+// sebagai argumennya (jumlah dari argumen tidak ditentukan).
 func sum(nums ...int) {
 	fmt.Print(nums, " ")
 	total := 0
@@ -20,14 +18,14 @@ func sum(nums ...int) {
 
 func main() {
 
-	// Variadic functions can be called in the usual way
-	// with individual arguments.
+	// Fungsi variadic bisa dipanggil seperti memanggil fungsi biasa
+	// tapi dengan jumlah argumen yang bisa berbeda-beda.
 	sum(1, 2)
 	sum(1, 2, 3)
 
-	// If you already have multiple args in a slice,
-	// apply them to a variadic function using
-	// `func(slice...)` like this.
+	// Jika kita sudah mempunyai sejumlah argumen dalam bentuk slice,
+	// kita bisa mengaplikasikannya dalam sebuah pemanggilan fungsi variadic
+	// dengan menggunakan sintaks `func(slice...)` .
 	nums := []int{1, 2, 3, 4}
 	sum(nums...)
 }
--- examples/variadic-functions/variadic-functions.sh
+++ examples/variadic-functions/variadic-functions.sh
@@ -3,5 +3,6 @@ $ go run variadic-functions.go
 [1 2 3] 6
 [1 2 3 4] 10
 
-# Another key aspect of functions in Go is their ability
-# to form closures, which we'll look at next.
+# Aspek utama lainnya dari sebuah fungsi di Go adalah 
+# kemapuan untuk membuat _closure_, yang akan kita
+# liat di bagian berikutnya.
