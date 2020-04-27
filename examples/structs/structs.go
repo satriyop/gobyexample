bc09887a0ab15fd62e9404a460d9e6a041041933
Satriyo Pamungkas <satriyopamungkas@gmail.com>
2020-04-27 21:45:12 +0700

    ex:struct

--- examples/structs/structs.go
+++ examples/structs/structs.go
@@ -1,21 +1,21 @@
-// Go's _structs_ are typed collections of fields.
-// They're useful for grouping data together to form
-// records.
-
+// Di bahasa pemrograman Go, _struct_ adalah sebuah _type_
+// yang berupa kumpulan dari _field-field_.
+// Struct berguna untuk mengelompokkan data-data
+// sehingga bisa disimpan.
 package main
 
 import "fmt"
 
-// This `person` struct type has `name` and `age` fields.
+// Type dari _struct_ `person` ini mempunyai _field_ `name` dan `age`.
 type person struct {
 	name string
 	age  int
 }
 
-// NewPerson constructs a new person struct with the given name
+// Fungsi NewPerson ini akan menbentuk _struct_ person dengan argumen name.
 func NewPerson(name string) *person {
-	// You can safely return a pointer to local variable
-	// as a local variable will survive the scope of the function.
+	// Kita bisa mengembalikan sebuah pointer pada variabel lokal
+	// karena variabel lokal ini akan masih dalam _scope_ fungsi.
 	p := person{name: name}
 	p.age = 42
 	return &p
@@ -23,31 +23,35 @@ func NewPerson(name string) *person {
 
 func main() {
 
-	// This syntax creates a new struct.
+	// Sintaks ini akan menciptakan sebuah struct baru.
 	fmt.Println(person{"Bob", 20})
 
-	// You can name the fields when initializing a struct.
+	// Kita bisa menuliskan nama field ketika menginisiasi
+	// sebuah struct.
 	fmt.Println(person{name: "Alice", age: 30})
 
-	// Omitted fields will be zero-valued.
+	// Field yang tidak diberi nilai akan `zero-valued`
+	// (yang nilainya akan tergantung dengan tipenya)
 	fmt.Println(person{name: "Fred"})
 
-	// An `&` prefix yields a pointer to the struct.
+	// Prefiks `&` akan menghasilkan sebuah pointer
+	// terhadap struct tersebut.
 	fmt.Println(&person{name: "Ann", age: 40})
 
-	// It's idiomatic to encapsulate new struct creation in constructor functions
+	// Mengenkapsulasi pembuatan struct baru dalam sebuah fungsi _constructor_
+	// disarankan (idiomatic).
 	fmt.Println(NewPerson("Jon"))
 
-	// Access struct fields with a dot.
+	// Gunakan dot (.) untuk mengakses field dari sebuah struct.
 	s := person{name: "Sean", age: 50}
 	fmt.Println(s.name)
 
-	// You can also use dots with struct pointers - the
-	// pointers are automatically dereferenced.
+	// Kita juga bisa menggunakan dot dalam pointer struct.
+	// Pointer tersebut akan secara otomatis di-_dereference_.
 	sp := &s
 	fmt.Println(sp.age)
 
-	// Structs are mutable.
+	// Struct merupakan tipe data yang _mutable_ (bisa diubah).
 	sp.age = 51
 	fmt.Println(sp.age)
 }
