0f7228e7bbd2f3118afbe72ce5d682f6b1ed9958
Satriyo Pamungkas <satriyopamungkas@gmail.com>
2020-09-28 01:12:02 +0700

    string function

--- examples/string-functions/string-functions.go
+++ examples/string-functions/string-functions.go
@@ -1,7 +1,3 @@
-// The standard library's `strings` package provides many
-// useful string-related functions. Here are some examples
-// to give you a sense of the package.
-
 // Pustaka standar `strings` menyeiakan berbagai fungsi
 // yang berkaitan dengan string. Di sini beberapa contoh
 // akan menunjukkan penggunakan dari package ini.
@@ -12,23 +8,11 @@ import (
 	s "strings"
 )
 
-// We alias `fmt.Println` to a shorter name as we'll use
-// it a lot below.
-
 // Kita meng-aliaskan `fmt.Println` ke dalam nama yang
 // lebih pendek karena kita akan sering menggunakannya.
 var p = fmt.Println
 
 func main() {
-
-	// Here's a sample of the functions available in
-	// `strings`. Since these are functions from the
-	// package, not methods on the string object itself,
-	// we need pass the string in question as the first
-	// argument to the function. You can find more
-	// functions in the [`strings`](http://golang.org/pkg/strings/)
-	// package docs.
-
 	// Di sini adalah beberapa contoh fungsi yang ada
 	// pada `strings`. Karena fungsi-fungsi ini berasal
 	// dari package, bukan merupakan method dari object
@@ -50,10 +34,6 @@ func main() {
 	p("ToUpper:   ", s.ToUpper("test"))
 	p()
 
-	// Not part of `strings`, but worth mentioning here, are
-	// the mechanisms for getting the length of a string in
-	// bytes and getting a byte by index.
-
 	// Bukan merupakan bagian dari  package `strings`, tapi
 	// cukup penting disebutkan disini adalah mekanisme untuk
 	// mendapatkan panjang dari sebuah string dalam bytes dan
@@ -61,13 +41,6 @@ func main() {
 	p("Len: ", len("hello"))
 	p("Char:", "hello"[1])
 }
-
-// Note that `len` and indexing above work at the byte level.
-// Go uses UTF-8 encoded strings, so this is often useful
-// as-is. If you're working with potentially multi-byte
-// characters you'll want to use encoding-aware operations.
-// See [strings, bytes, runes and characters in Go](https://blog.golang.org/strings)
-// for more information.
 
 // Perhatikan bahwa `len` dan indexing di atas bekerja pada
 // level byte. Go menggunakan UTF-8 encoded string, sehingga
